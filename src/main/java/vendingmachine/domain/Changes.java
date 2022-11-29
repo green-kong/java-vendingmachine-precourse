22697ba8532aefb252742cce6fccbfe4208e4f19
dev_kong <ahdjd5@gmail.com>
2022-11-29 14:17:39 +0900

    feat(Changes): add createRandomCoin method

--- src/main/java/vendingmachine/domain/Changes.java
+++ src/main/java/vendingmachine/domain/Changes.java
@@ -6,11 +6,22 @@ import java.util.Arrays;
 import java.util.EnumMap;
 
 public class Changes {
-    EnumMap<Coin,Integer> changeMap;
+    EnumMap<Coin, Integer> changeMap;
 
-    public Changes(EnumMap<Coin,Integer> changeMap) {
+    public Changes(EnumMap<Coin, Integer> changeMap) {
         Arrays.stream(Coin.values())
-                .forEach(coin -> changeMap.put(coin, 0 ));
+                .forEach(coin -> changeMap.put(coin, 0));
         this.changeMap = changeMap;
+    }
+
+    public void createRandomCoin(int money) {
+        while (money > 0) {
+            int randomAmount = Coin.getRandomAmount();
+            if (money - randomAmount >= 0) {
+                money -= randomAmount;
+                Coin coin = Coin.getCoinByAmount(randomAmount);
+                changeMap.put(coin, changeMap.get(coin) + 1);
+            }
+        }
     }
 }
