e10d5c661c8dfb4049d953ab050ed2d369190907
dev_kong <ahdjd5@gmail.com>
2022-11-29 15:55:37 +0900

    feat(Validator): add function to check product input format
    대괄호로 감싸져있지 않으면 예외처리,
    제품명, 가격, 수량으로 이뤄져있지 않으면 예외처리

--- src/main/java/vendingmachine/utils/Validator.java
+++ src/main/java/vendingmachine/utils/Validator.java
@@ -6,4 +6,19 @@ public class Validator {
             throw new IllegalArgumentException("10원 단위로 입력가능합니다.");
         }
     }
+
+    public static void checkProductInputFormat(String productInput) {
+        String[] inputs = productInput.split("");
+        if (!inputs[0].equals("[") || !inputs[inputs.length - 1].equals("]")) {
+            throw new IllegalArgumentException("제품의 상세내용을 [] 안에 입력해주세요.");
+        }
+    }
+
+    public static void checkProductInputLength(String productInput) {
+        String[] inputs = productInput.split(",");
+        if(inputs.length != 3) {
+            throw new IllegalArgumentException("제품명, 가격, 수량을 입력해주세요.");
+        }
+
+    }
 }
