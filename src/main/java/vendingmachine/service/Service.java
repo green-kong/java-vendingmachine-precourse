71e75f6e76b76459d6835cc4ff0dc09524ce1eb4
dev_kong <ahdjd5@gmail.com>
2022-11-29 16:00:03 +0900

    feat(Service): add createProducts method

--- src/main/java/vendingmachine/service/Service.java
+++ src/main/java/vendingmachine/service/Service.java
@@ -2,13 +2,18 @@ package vendingmachine.service;
 
 import vendingmachine.Coin;
 import vendingmachine.domain.Changes;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.Products;
 import vendingmachine.utils.Converter;
+import vendingmachine.utils.Validator;
 
 import java.util.EnumMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Service {
     Changes changes = new Changes(new EnumMap<>(Coin.class));
+    Products products;
 
     public void createHoldingCoin(String holdingMoneyInput) {
         int holdingMoney = Converter.toIntFromString(holdingMoneyInput);
@@ -17,5 +22,22 @@ public class Service {
 
     public List<String> getHoldingCoinResult() {
         return changes.getHoldingCoin();
+    }
+
+    public void createProducts(String productInput) {
+        List<String> productInputs = Converter.parseProductInput(productInput);
+        validateProductInputs(productInputs);
+        List<List<String>> productDescriptions = Converter.parseProductDescriptions(productInputs);
+        List<Product> products = productDescriptions.stream().
+                map(Product::new)
+                .collect(Collectors.toList());
+        this.products = new Products(products);
+    }
+
+    private void validateProductInputs(List<String> productInputs) {
+        productInputs.forEach(input -> {
+            Validator.checkProductInputFormat(input);
+            Validator.checkProductInputLength(input);
+        });
     }
 }
