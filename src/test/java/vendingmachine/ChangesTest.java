0bc168919915090164cbbbebceafa24de219f4a9
dev_kong <ahdjd5@gmail.com>
2022-11-29 14:55:00 +0900

    test(Changes): add test code to check money input
    입력받은 값이 10원단위로 나눠떨어지지 않을때의 예외 처리

--- src/test/java/vendingmachine/ChangesTest.java
+++ src/test/java/vendingmachine/ChangesTest.java
@@ -1,8 +1,13 @@
 package vendingmachine;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import vendingmachine.domain.Changes;
 
 import java.util.EnumMap;
@@ -21,5 +26,13 @@ public class ChangesTest {
     void checkRandomCoin() {
         int input = 4500;
         changes.createRandomCoin(input);
+    }
+
+    @DisplayName("입력받은 금액이 10원단위로 나눠떨어지지 않을경우 예외처리")
+    @ParameterizedTest(name = "{displayName} input = {0}")
+    @ValueSource(ints = {1, 11, 21})
+    void checkMoneyUnit(int input) {
+        assertThatThrownBy(() -> changes.createRandomCoin(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
