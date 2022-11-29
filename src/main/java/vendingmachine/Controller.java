6ef4f0d9600c03cc3904db2d55ad31e205708c6b
dev_kong <ahdjd5@gmail.com>
2022-11-29 13:41:05 +0900

    feat: set controller

---
+++ src/main/java/vendingmachine/Controller.java
@@ -0,0 +1,21 @@
+package vendingmachine;
+
+public class Controller {
+    public void run() {
+        setVendingMachine();
+        purchaseProduct();
+        getChanges();
+    }
+
+    public void setVendingMachine() {
+
+    }
+
+    public void purchaseProduct() {
+
+    }
+
+    public void getChanges() {
+
+    }
+}
