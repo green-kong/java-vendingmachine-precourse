1f166af02b85051644ac1c5ae750987269cb4c65
dev_kong <ahdjd5@gmail.com>
2022-11-29 17:05:50 +0900

    feat(Products): add getProductPrice method

--- src/main/java/vendingmachine/domain/Products.java
+++ src/main/java/vendingmachine/domain/Products.java
@@ -8,4 +8,20 @@ public class Products {
     public Products(List<Product> products) {
         this.products = products;
     }
+
+    public int getProductPrice(String productName) {
+        try {
+            Product product = getProductByName(productName);
+            return product.getPrice();
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException("존재하지 않는 제품명입니다.");
+        }
+    }
+
+    private Product getProductByName(String productName) {
+            return products.stream()
+                    .filter(productInfo -> productInfo.getName().equals(productName))
+                    .findFirst()
+                    .orElse(null);
+    }
 }
