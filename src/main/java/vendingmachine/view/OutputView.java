3c5b6f28c05b3a74ba96aa3e01ff3cf522eeea6f
dev_kong <ahdjd5@gmail.com>
2022-11-29 14:42:11 +0900

    feat(OutputView): add printHoldingCoin method

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,9 +1,16 @@
 package vendingmachine.view;
 
+import java.util.List;
+
 public class OutputView {
     private final static String ERROR_HEADER = "[ERROR]";
 
     public void printErrorMessage(String message) {
         System.out.println(ERROR_HEADER + message + "\n");
+    }
+
+    public void printHoldingCoin(List<String> holdingCoins) {
+        System.out.println("\n자판기가 보유한 동전");
+        holdingCoins.forEach(System.out::println);
     }
 }
