fcd51d514bc78052a15c2ec942b78117480d0b86
dev_kong <ahdjd5@gmail.com>
2022-11-29 16:28:00 +0900

    feat(VendingMachine): add depositMoney method

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,5 +1,7 @@
 package vendingmachine.domain;
 
+import vendingmachine.utils.Validator;
+
 public class VendingMachine {
     private final Changes changes;
     private final Products products;
@@ -8,5 +10,14 @@ public class VendingMachine {
     public VendingMachine(Changes changes, Products products) {
         this.changes = changes;
         this.products = products;
+    }
+
+    public void depositMoney(int depositAmount) {
+        validateDepositAmount(depositAmount);
+        this.deposit = depositAmount;
+    }
+
+    public void validateDepositAmount(int depositAmount) {
+        Validator.isAcceptableAmount(depositAmount);
     }
 }
