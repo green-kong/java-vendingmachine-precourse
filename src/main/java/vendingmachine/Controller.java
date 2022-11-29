2870100248984b5272120ffed597e832a41d46bd
dev_kong <ahdjd5@gmail.com>
2022-11-29 14:25:30 +0900

    feat(Controller): add function if exception occurs request input again

--- src/main/java/vendingmachine/Controller.java
+++ src/main/java/vendingmachine/Controller.java
@@ -2,10 +2,12 @@ package vendingmachine;
 
 import vendingmachine.service.Service;
 import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
 
 public class Controller {
     private final Service service = new Service();
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     public void run() {
         setVendingMachine();
@@ -26,7 +28,12 @@ public class Controller {
     }
 
     private void setHoldingMoney() {
-        String holdingMoneyInput = inputView.getHoldingMoneyInput();
-        service.createHoldingCoin(holdingMoneyInput);
+        try {
+            String holdingMoneyInput = inputView.getHoldingMoneyInput();
+            service.createHoldingCoin(holdingMoneyInput);
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            setHoldingMoney();
+        }
     }
 }
