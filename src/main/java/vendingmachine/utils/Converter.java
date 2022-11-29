85707b898728e01b4d923cb76ce2d5a4ed55d653
dev_kong <ahdjd5@gmail.com>
2022-11-29 15:05:23 +0900

    feat(Converter): add parseProductInput method
    사용자로부터 입력받은 값을 세미콜론(;)으로 구분하여 리스트로 변환 후 반환

--- src/main/java/vendingmachine/utils/Converter.java
+++ src/main/java/vendingmachine/utils/Converter.java
@@ -1,5 +1,8 @@
 package vendingmachine.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Converter {
     public static int toIntFromString(String input) {
         try {
@@ -7,5 +10,9 @@ public class Converter {
         } catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException("숫자만 입력가능합니다.");
         }
+    }
+
+    public static List<String> parseProductInput(String productInput) {
+        return Arrays.asList(productInput.split(";"));
     }
 }
